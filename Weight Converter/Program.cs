b71119fd36b0851257bdf2d86d5bb284aa6ad819
AkaMICKY <akaastroyx@gmail.com>
2025-09-07 20:08:47 +0100

    Updated code to use Convert method for input

--- Weight Converter/Program.cs
+++ Weight Converter/Program.cs
@@ -13,7 +13,7 @@ namespace Weight_Converter
             Console.WriteLine("Hello, I Convert Your Weight in Kg(Kilograms) to Lbs(Pounds)\nSo lets Get Started,");
             Console.Write("Whats Your Weight(Kg)? : "); //Collects User Info
             string Weight = Console.ReadLine(); 
-            double WEIGHT = double.Parse(Weight); //Coverts User Input(String) to Decimal For Calculation
+            double WEIGHT = Convert.ToInt32(Weight); //Coverts User Input(String) to Decimal For Calculation
             double Weight_lbs = WEIGHT * 2.20462; //Multiply Weight(kg) to Weight(lbs) by multiplying by 2.20462
             double Weight_lbs_round = Math.Round(Weight_lbs, 2); //Rounds Weight to 2 Decimal Places
             Console.WriteLine("According to My Calculations You Should Weigh " + Weight_lbs_round + " lbs");
